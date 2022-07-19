ef42a3fb5d76d7f4ee3c161ad5fb17a7e7c2e4ae
Mackenzie Hicks <mackenziehicks@MackenziesMBP3.lan>
2022-07-19 12:25:22 -0700

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+            <img src={"https://growtherapy.com/wp-content/uploads/2022/04/Full-Logo.svg"} className="App-logo" alt="logo" />
+            <p>
+                Grow Take Home
+            </p>
+        </header>);
+  }
+}
+
+export default Header;
