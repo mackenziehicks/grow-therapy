7bdbec06886aac56fad803af8bcd09d869f0cd0c
mackenziehicks1 <mackenzie@hu.man.ai>
2022-07-20 18:10:49 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from './app/components/frame/Header.tsx';
 import Dashboard from './app/views/dashboard/Dashboard.tsx';
-import './assets/tailwind.css';
 import './App.css';
 
 function App() {
